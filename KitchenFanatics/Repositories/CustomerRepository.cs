2f2a78504389b47acb2c84d30168b31c086d0fad
Esben Dam Cramer <edcr49481@edu.ucl.dk>
2022-11-21 10:54:50 +0100

    Connected the Database

---
+++ KitchenFanatics/Repositories/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenFanatics.Repositories
+{
+    internal class CustomerRepository
+    {
+    }
+}

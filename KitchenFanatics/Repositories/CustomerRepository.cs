e026551a36d08f13cb2172b131357d6a7a9c5098
Mikkel Lill Hagedorn Thusgaard <mlht49473@edu.ucl.dk>
2022-11-22 12:55:33 +0100

    Updated the CreateCustomer form, CustomerOverview. Made the basefunctions of the CustomerRepository.

--- KitchenFanatics/Repositories/CustomerRepository.cs
+++ KitchenFanatics/Repositories/CustomerRepository.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KitchenFanatics.Models;
+using KitchenFanatics.Forms;
+using KitchenFanatics.Properties;
+using KitchenFanatics.Services;
 
 namespace KitchenFanatics.Repositories
 {
     internal class CustomerRepository
     {
+        public List<Customer> GetAllCustomers()
+        {
+            Database.KitchenFanaticDBDataContext customerDataContext = new Database.KitchenFanaticDBDataContext();
+
+            var result = new List<Customer>();
+
+            var AllCustomers = customerDataContext.Customers;
+
+            foreach (var customer in AllCustomers)
+            {
+                var customers = new Customer(customer.FirstName, customer.LastName, customer.Email, customer.PhoneNumber, customer.CustomerAddress, customer.CustomerID);
+
+                result.Add(customers);
+            }
+            return result;
+        }
     }
 }

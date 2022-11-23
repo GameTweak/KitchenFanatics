55837c4ee53a5c70e2ef95dbb3d408772aae0f4f
Mikkel Lill Hagedorn Thusgaard <mlht49473@edu.ucl.dk>
2022-11-23 12:47:30 +0100

    Finished - the customerRepository - the customerService

--- KitchenFanatics/Services/CustomerService.cs
+++ KitchenFanatics/Services/CustomerService.cs
@@ -8,6 +8,28 @@ namespace KitchenFanatics.Services
 {
     internal class CustomerService
     {
+        public void createCustomer(Models.Customer currentCustomer)
+        {
+            var CustomerRepository = new Repositories.CustomerRepository();
+            CustomerRepository.createCustomer(currentCustomer);
+        }
 
+        public void updateCustomer(Models.Customer currentCustomer)
+        {
+            var customerRepository = new Repositories.CustomerRepository();
+            customerRepository.updateCustomer(currentCustomer);
+        }
+
+        private void deleteCustomer(Models.Customer currentCustomer)
+        {
+            var customerRepository = new Repositories.CustomerRepository();
+            customerRepository.deleteCustomer(currentCustomer);
+        }
+
+        private List<Models.Customer> GetCustomers()
+        {
+            var customerRepository = new Repositories.CustomerRepository();
+            return customerRepository.GetAllCustomers();
+        }
     }
 }

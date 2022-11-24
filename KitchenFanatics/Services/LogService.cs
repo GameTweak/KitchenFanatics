e24f45a3ceb758711972b5a41eaa2dbbaa399f13
Thomas Lind <thli49474@edu.ucl.dk>
2022-11-24 11:53:59 +0100

    LogService

---
+++ KitchenFanatics/Services/LogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitchenFanatics.Services
+{
+    public class LogService
+    {
+        public void LogError(Exception ex)
+        {
+            MessageBox.Show(ex.Message + ", check logging for more info");
+            Console.WriteLine(ex.ToString());
+        }
+
+        public void LogError(Exception ex, string message)
+        {
+            MessageBox.Show(ex.Message + ", " + message);
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}

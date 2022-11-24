59cc11b4e5bcef2626ffb86129f2a07290687e92
GameTweak <esbendam@live.dk>
2022-11-24 23:05:12 +0100

    Har lavet et filter system samt en ny model til at opbevarer SaleLines

---
+++ KitchenFanatics/Models/SaleLine.cs
@@ -0,0 +1,32 @@
+﻿using KitchenFanatics.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenFanatics.Models
+{
+    public class SaleLine
+    {
+        public SaleLine(int? saleLineID, int? itemNR, int? amount, decimal? price, int? saleID)
+        {
+            SaleLineID = saleLineID;
+            ItemNR = itemNR;
+            Amount = amount;
+            Price = price;
+            SaleID = saleID;
+        }
+
+        public int? SaleLineID { get; set; }
+
+        public int? ItemNR { get; set; }
+
+        public int? Amount { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public int? SaleID { get; set; }
+
+    }
+}

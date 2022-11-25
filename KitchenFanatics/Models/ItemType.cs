2de9304a1329eefd198da598d0d137a3270c1d31
Thomas Lind <thli49474@edu.ucl.dk>
2022-11-25 13:39:59 +0100

    Intellectual counselling, denne version er alle de modeller, services og repositories som er nødvendige til selve programmet

---
+++ KitchenFanatics/Models/ItemType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenFanatics.Models
+{
+    public class ItemType
+    {
+        //a constructor is created for the ItemType class, ? allows null
+        public ItemType(string typeName, int? uniqueAmount, int id)
+        {
+            this.TypeName = typeName;
+            this.UniqueAmount = uniqueAmount;
+            this.id = id;
+        }
+
+        //the class is assigned fields, which are all made public 
+        public string TypeName { get; set; }
+        public int? UniqueAmount { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ KitchenFanatics/Services/LineReadService.cs
@@ -0,0 +1,55 @@
+﻿using KitchenFanatics.Models;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenFanatics.Services
+{
+    public class LineReadService
+    {
+        //A list is established, so it can be reffered to from any other place int the program
+        public List<ItemType> AllTypes { get; set; }
+
+        //A constructor is created for the ItemType class
+        public LineReadService()
+        {
+            //A cennection to ItemTypeRepository is made
+            var itemTypeRepository = new Repositories.ItemTypeRepository();
+            //AllTypes is set to include all item types
+            AllTypes = itemTypeRepository.GetAllItemTypes();
+        }
+
+        /// <summary>
+        /// Reads a string while looking for item types
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public List<int> ReadForType(string text)
+        {
+            //Makes a new list of int named result
+            var result = new List<int>();
+
+            //Makes a new string that is the original text but in uppercase
+            var Text = text.ToUpper();
+
+            //A foreach that repeats for each type in AllTypes
+            foreach (var type in AllTypes)
+            {
+                //Check if the itemtypes name is in the text
+                if (Text.Contains(type.TypeName.ToUpper()) == true)
+                {
+                    //makes a temp variable, and sets it to hold the itemtype that matches the text
+                    var temp = AllTypes.Find(itm => itm.TypeName.ToUpper() == type.TypeName.ToUpper());
+                    //adds the itemtypes id to result
+                    result.Add(temp.id);
+                }
+            }
+
+            //returns result
+            return result;
+        }
+    }
+}

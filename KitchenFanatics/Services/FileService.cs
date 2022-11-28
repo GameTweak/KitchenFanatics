e3d154466724970a8c359bc9909cdd153084ed01
GameTweak <esbendam@live.dk>
2022-11-28 22:31:28 +0100

    Begyndt at lave noget til udskirivning

--- KitchenFanatics/Services/FileService.cs
+++ KitchenFanatics/Services/FileService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace KitchenFanatics.Services
 {
@@ -12,7 +13,14 @@ namespace KitchenFanatics.Services
 
         public void CreateFile(List<SaleHistory> sale)
         {
-            
+            Word.Application winword = new Word.Application();
+
+            winword.ShowAnimation = false;
+            winword.Visible = false;
+
+            object missing = System.Reflection.Missing.Value;
+
+            //Word.Document = winword.Documents.Add(ref missing)
         } 
 
     }

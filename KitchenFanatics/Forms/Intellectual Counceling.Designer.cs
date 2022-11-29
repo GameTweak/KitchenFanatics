dc4519a0e431434b6969411751737eb26cb01a66
Thomas Lind <thli49474@edu.ucl.dk>
2022-11-29 10:51:50 +0100

    Intelligent counseling, now only untested

--- KitchenFanatics/Forms/Intellectual Counceling.Designer.cs
+++ KitchenFanatics/Forms/Intellectual Counceling.Designer.cs
@@ -28,12 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_question = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.btn_skip = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.lb_number1 = new System.Windows.Forms.Label();
+            this.lb_number2 = new System.Windows.Forms.Label();
+            this.lb_number3 = new System.Windows.Forms.Label();
+            this.txt_number1 = new System.Windows.Forms.TextBox();
+            this.txt_number2 = new System.Windows.Forms.TextBox();
+            this.txt_number3 = new System.Windows.Forms.TextBox();
+            this.txt_number6 = new System.Windows.Forms.TextBox();
+            this.txt_number5 = new System.Windows.Forms.TextBox();
+            this.txt_number4 = new System.Windows.Forms.TextBox();
+            this.lb_number6 = new System.Windows.Forms.Label();
+            this.lb_number5 = new System.Windows.Forms.Label();
+            this.lb_number4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_question
+            // 
+            this.lb_question.AutoSize = true;
+            this.lb_question.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_question.Location = new System.Drawing.Point(12, 9);
+            this.lb_question.Name = "lb_question";
+            this.lb_question.Size = new System.Drawing.Size(217, 25);
+            this.lb_question.TabIndex = 0;
+            this.lb_question.Text = "Question placeholder";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(12, 66);
+            this.txt_input.Multiline = true;
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(311, 160);
+            this.txt_input.TabIndex = 1;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_submit.Location = new System.Drawing.Point(12, 290);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(92, 54);
+            this.btn_submit.TabIndex = 2;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // btn_skip
+            // 
+            this.btn_skip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_skip.Location = new System.Drawing.Point(110, 290);
+            this.btn_skip.Name = "btn_skip";
+            this.btn_skip.Size = new System.Drawing.Size(92, 54);
+            this.btn_skip.TabIndex = 3;
+            this.btn_skip.Text = "Skip";
+            this.btn_skip.UseVisualStyleBackColor = true;
+            this.btn_skip.Click += new System.EventHandler(this.btn_skip_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_cancel.Location = new System.Drawing.Point(208, 290);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(92, 54);
+            this.btn_cancel.TabIndex = 4;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // lb_number1
+            // 
+            this.lb_number1.AutoSize = true;
+            this.lb_number1.Location = new System.Drawing.Point(25, 69);
+            this.lb_number1.Name = "lb_number1";
+            this.lb_number1.Size = new System.Drawing.Size(79, 16);
+            this.lb_number1.TabIndex = 6;
+            this.lb_number1.Text = "Max bredde";
+            this.lb_number1.Visible = false;
+            // 
+            // lb_number2
+            // 
+            this.lb_number2.AutoSize = true;
+            this.lb_number2.Location = new System.Drawing.Point(25, 106);
+            this.lb_number2.Name = "lb_number2";
+            this.lb_number2.Size = new System.Drawing.Size(69, 16);
+            this.lb_number2.TabIndex = 7;
+            this.lb_number2.Text = "Max højde";
+            this.lb_number2.Visible = false;
+            // 
+            // lb_number3
+            // 
+            this.lb_number3.AutoSize = true;
+            this.lb_number3.Location = new System.Drawing.Point(25, 142);
+            this.lb_number3.Name = "lb_number3";
+            this.lb_number3.Size = new System.Drawing.Size(74, 16);
+            this.lb_number3.TabIndex = 8;
+            this.lb_number3.Text = "Max dybde";
+            this.lb_number3.Visible = false;
+            // 
+            // txt_number1
+            // 
+            this.txt_number1.Location = new System.Drawing.Point(110, 66);
+            this.txt_number1.Name = "txt_number1";
+            this.txt_number1.Size = new System.Drawing.Size(61, 22);
+            this.txt_number1.TabIndex = 9;
+            this.txt_number1.Visible = false;
+            this.txt_number1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_number_KeyPress);
+            // 
+            // txt_number2
+            // 
+            this.txt_number2.Location = new System.Drawing.Point(110, 103);
+            this.txt_number2.Name = "txt_number2";
+            this.txt_number2.Size = new System.Drawing.Size(61, 22);
+            this.txt_number2.TabIndex = 10;
+            this.txt_number2.Visible = false;
+            this.txt_number2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_number_KeyPress);
+            // 
+            // txt_number3
+            // 
+            this.txt_number3.Location = new System.Drawing.Point(110, 139);
+            this.txt_number3.Name = "txt_number3";
+            this.txt_number3.Size = new System.Drawing.Size(61, 22);
+            this.txt_number3.TabIndex = 11;
+            this.txt_number3.Visible = false;
+            this.txt_number3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_number_KeyPress);
+            // 
+            // txt_number6
+            // 
+            this.txt_number6.Location = new System.Drawing.Point(110, 247);
+            this.txt_number6.Name = "txt_number6";
+            this.txt_number6.Size = new System.Drawing.Size(61, 22);
+            this.txt_number6.TabIndex = 17;
+            this.txt_number6.Visible = false;
+            this.txt_number6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_number_KeyPress);
+            // 
+            // txt_number5
+            // 
+            this.txt_number5.Location = new System.Drawing.Point(110, 211);
+            this.txt_number5.Name = "txt_number5";
+            this.txt_number5.Size = new System.Drawing.Size(61, 22);
+            this.txt_number5.TabIndex = 16;
+            this.txt_number5.Visible = false;
+            this.txt_number5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_number_KeyPress);
+            // 
+            // txt_number4
+            // 
+            this.txt_number4.Location = new System.Drawing.Point(110, 174);
+            this.txt_number4.Name = "txt_number4";
+            this.txt_number4.Size = new System.Drawing.Size(61, 22);
+            this.txt_number4.TabIndex = 15;
+            this.txt_number4.Visible = false;
+            this.txt_number4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_number_KeyPress);
+            // 
+            // lb_number6
+            // 
+            this.lb_number6.AutoSize = true;
+            this.lb_number6.Location = new System.Drawing.Point(25, 250);
+            this.lb_number6.Name = "lb_number6";
+            this.lb_number6.Size = new System.Drawing.Size(70, 16);
+            this.lb_number6.TabIndex = 14;
+            this.lb_number6.Text = "Min dybde";
+            this.lb_number6.Visible = false;
+            // 
+            // lb_number5
+            // 
+            this.lb_number5.AutoSize = true;
+            this.lb_number5.Location = new System.Drawing.Point(25, 214);
+            this.lb_number5.Name = "lb_number5";
+            this.lb_number5.Size = new System.Drawing.Size(65, 16);
+            this.lb_number5.TabIndex = 13;
+            this.lb_number5.Text = "Min højde";
+            this.lb_number5.Visible = false;
+            // 
+            // lb_number4
+            // 
+            this.lb_number4.AutoSize = true;
+            this.lb_number4.Location = new System.Drawing.Point(25, 177);
+            this.lb_number4.Name = "lb_number4";
+            this.lb_number4.Size = new System.Drawing.Size(75, 16);
+            this.lb_number4.TabIndex = 12;
+            this.lb_number4.Text = "Min bredde";
+            this.lb_number4.Visible = false;
+            // 
+            // Intellectual_Counceling
+            // 
+            this.AcceptButton = this.btn_submit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.CancelButton = this.btn_cancel;
+            this.ClientSize = new System.Drawing.Size(560, 356);
+            this.Controls.Add(this.txt_number6);
+            this.Controls.Add(this.txt_number5);
+            this.Controls.Add(this.txt_number4);
+            this.Controls.Add(this.lb_number6);
+            this.Controls.Add(this.lb_number5);
+            this.Controls.Add(this.lb_number4);
+            this.Controls.Add(this.txt_number3);
+            this.Controls.Add(this.txt_number2);
+            this.Controls.Add(this.txt_number1);
+            this.Controls.Add(this.lb_number3);
+            this.Controls.Add(this.lb_number2);
+            this.Controls.Add(this.lb_number1);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_skip);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.lb_question);
+            this.Name = "Intellectual_Counceling";
             this.Text = "Intellectual_Counceling";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb_question;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Button btn_skip;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Label lb_number1;
+        private System.Windows.Forms.Label lb_number2;
+        private System.Windows.Forms.Label lb_number3;
+        private System.Windows.Forms.TextBox txt_number1;
+        private System.Windows.Forms.TextBox txt_number2;
+        private System.Windows.Forms.TextBox txt_number3;
+        private System.Windows.Forms.TextBox txt_number6;
+        private System.Windows.Forms.TextBox txt_number5;
+        private System.Windows.Forms.TextBox txt_number4;
+        private System.Windows.Forms.Label lb_number6;
+        private System.Windows.Forms.Label lb_number5;
+        private System.Windows.Forms.Label lb_number4;
     }
 }
--- KitchenFanatics/Forms/Intellectual Counceling.cs
+++ KitchenFanatics/Forms/Intellectual Counceling.cs
@@ -12,9 +12,295 @@ namespace KitchenFanatics.Forms
 {
     public partial class Intellectual_Counceling : Form
     {
+        // makes a series of variables that i am going use later in the code
+        public int QuestionNumber { get; set; }
+        public Models.Filter CurrentFilter { get; set; }
+        // ValidDecimal is used when checking wether or not a string can be converted to a decimal
+        public bool ValidDecimal { get; set; }
+
+        /// <summary>
+        /// A constructer that starts when the form is innitialized
+        /// </summary>
         public Intellectual_Counceling()
         {
             InitializeComponent();
+
+            // Sets QuestionNumber to 0 (the first question)
+            QuestionNumber = 0;
+
+            // Sets the default values for CurrentFilter
+            CurrentFilter = new Models.Filter("",null,null,null,null,null,null,null,null,null);
+
+            // Calls the method SelectQuestion
+            SelectQuestion();
+        }
+
+        /// <summary>
+        /// Sets the UI to match the currently chosen question
+        /// </summary>
+        private void SelectQuestion()
+        {
+            // A switch that checks what number question is the current one
+            switch (QuestionNumber)
+            {
+                case 0:
+                    QuestionType();
+                    break;
+
+                case 1:
+                    QuestionSize();
+                    break;
+
+                case 2:
+                    QuestionWeight();
+                    break;
+
+                case 3:
+                    QuestionPrice();
+                    break;
+
+                case 4:
+                    // When all questions have been answered, sets dialogresult to OK so the data can be taken to another form
+                    // And then closes the form
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Submits the data for the current question
+        /// </summary>
+        private void Submit()
+        {
+            // A switch that checks what number question is being worked with
+            switch (QuestionNumber)
+            {
+                case 0:
+                    ValidDecimal = true;
+                    // Checks whether or not anything is written in the input
+                    if (txt_input.Text != "")
+                    {
+                        // Saves the input into the current filter
+                        CurrentFilter.Type = txt_input.Text;
+                    }
+                    break;
+
+                case 1:
+                    ValidDecimal = true;
+                    // Uses the method CheckTryParseStringToDecimal to save each values data into the current filter
+                    CurrentFilter.MaxHeight = CheckTryParseStringToDecimal("Max højde", txt_number1.Text);
+                    CurrentFilter.MaxWidth = CheckTryParseStringToDecimal("Max bredde", txt_number2.Text);
+                    CurrentFilter.MaxDepth = CheckTryParseStringToDecimal("Max dybde", txt_number3.Text);
+                    CurrentFilter.MinHeight = CheckTryParseStringToDecimal("Min højde", txt_number4.Text);
+                    CurrentFilter.MinWidth = CheckTryParseStringToDecimal("Min bredde", txt_number5.Text);
+                    CurrentFilter.MinDepth = CheckTryParseStringToDecimal("Min dybde", txt_number6.Text);
+                    break;
+
+                case 2:
+                    ValidDecimal = true;
+                    CurrentFilter.MaxWeight = CheckTryParseStringToDecimal("Max vægt", txt_number1.Text);
+                    break;
+
+                case 3:
+                    ValidDecimal = true;
+                    CurrentFilter.MaxPrice = CheckTryParseStringToDecimal("Max pris", txt_number1.Text);
+                    CurrentFilter.MinPrice = CheckTryParseStringToDecimal("Min pris", txt_number2.Text);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Sets the UI for the question about type
+        /// </summary>
+        private void QuestionType()
+        {
+            ClearUI();
+            lb_question.Text = "Hvilken type/typer produkter leder du efter";
+
+            txt_input.Visible = true;
+        }
+
+        /// <summary>
+        /// Sets the UI for the question about size
+        /// </summary>
+        private void QuestionSize()
+        {
+            ClearUI();
+            lb_question.Text = "Hvilke størrelse ønsker du";
+
+            txt_number1.Visible = true;
+            txt_number2.Visible = true;
+            txt_number3.Visible = true;
+            txt_number4.Visible = true;
+            txt_number5.Visible = true;
+            txt_number6.Visible = true;
+            lb_number1.Visible = true;
+            lb_number2.Visible = true;
+            lb_number3.Visible = true;
+            lb_number4.Visible = true;
+            lb_number5.Visible = true;
+            lb_number6.Visible = true;
+
+            lb_number1.Text = "Max højde";
+            lb_number2.Text = "Max bredde";
+            lb_number3.Text = "Max dybde";
+            lb_number4.Text = "Min højde";
+            lb_number5.Text = "Min bredde";
+            lb_number6.Text = "Min dybde";
+        }
+
+        /// <summary>
+        /// Sets the UI for the question about wheight
+        /// </summary>
+        private void QuestionWeight()
+        {
+            ClearUI();
+            lb_question.Text = "Hvor tung må produktet være";
+
+            txt_number1.Visible = true;
+            lb_number1.Visible = true;
+
+            lb_number1.Text = "Max vægt";
+        }
+
+        /// <summary>
+        /// Sets the UI for the question about price
+        /// </summary>
+        private void QuestionPrice()
+        {
+            ClearUI();
+            lb_question.Text = "Hvilken prisklasse leder du efter";
+
+            txt_number1.Visible = true;
+            txt_number2.Visible = true;
+            lb_number1.Visible = true;
+            lb_number2.Visible = true;
+
+            lb_number1.Text = "Max pris";
+            lb_number2.Text = "Min pris";
+        }
+
+        /// <summary>
+        /// When cancel is clicked, closes the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Sets what happens when one clicks on the submit button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            // Calls the submit method
+            Submit();
+
+            // Checks whether or not the convertion to decimal is valid, if it is then the program proceeds to next question
+            if (ValidDecimal == true)
+            {
+                QuestionNumber++;
+                SelectQuestion();
+            }
+        }
+
+        /// <summary>
+        /// Skips the question when skip is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_skip_Click(object sender, EventArgs e)
+        {
+            QuestionNumber++;
+            SelectQuestion();
+        }
+
+        /// <summary>
+        /// Limits the text inputs where numbers have to be inserted, so (almost) only numbers can be inserted
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txt_number_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Clears the UI
+        /// </summary>
+        private void ClearUI()
+        {
+            txt_input.Visible = false;
+            txt_number1.Visible = false;
+            txt_number2.Visible = false;
+            txt_number3.Visible = false;
+            txt_number4.Visible = false;
+            txt_number5.Visible = false;
+            txt_number6.Visible = false;
+            lb_number1.Visible = false;
+            lb_number2.Visible = false;
+            lb_number3.Visible = false;
+            lb_number4.Visible = false;
+            lb_number5.Visible = false;
+            lb_number6.Visible = false;
+
+            txt_input.Text = "";
+            txt_number1.Text = "";
+            txt_number2.Text = "";
+            txt_number3.Text = "";
+            txt_number4.Text = "";
+            txt_number5.Text = "";
+            txt_number6.Text = "";
+        }
+
+        /// <summary>
+        /// Checks whether or not a string can be converted into a valid decimal
+        /// </summary>
+        /// <param name="varName"></param>
+        /// <param name="from"></param>
+        /// <returns></returns>
+        private decimal? CheckTryParseStringToDecimal(string varName, string from)
+        {
+            // Checks that something has been written into the text, if not then return null
+            if (from == "")
+            {
+                return null;
+            }
+            // Checks that the string can be converted into a valid decimal, if yes, then returns that decimal
+            else if (decimal.TryParse(from, out decimal filler) && Convert.ToDecimal(from) > 0)
+            {
+                return Convert.ToDecimal(from);
+            }
+            // Checks wether or not the string can be converted into a negative decimal, if yes, then tell the user the decimal has to be possitive,
+            // sets ValidDecimal to false, and returns null
+            else if (decimal.TryParse(from, out decimal filler2) && Convert.ToDecimal(from) < 0)
+            {
+                MessageBox.Show(varName + " kan ikke være negativ");
+                ValidDecimal = false;
+                return null;
+            }
+            // The code will only end up here if some other problem occoured, it will tell the user the string could not be converted, set
+            // ValidDecimal to false, and return null
+            else
+            {
+                MessageBox.Show(varName + " kunne ikke konverteres til et decimaltal");
+                ValidDecimal = false;
+                return null;
+            }
         }
     }
 }
